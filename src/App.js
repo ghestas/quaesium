56dddbdf3fcb9f36c73ec8dd7c90d780e36956eb
ghestas <ghestas@ghestas.com>
2024-02-03 16:00:50 +1300

    changed build stuff

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Landing from './Landing'
 import Dashboard from './Dashboard';
 import Game from './Game';
 
+
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route path='/' element={ Math.random() < 0.5 ? <Landing /> : <Dashboard /> } />,
     <Route path='/game/:id' element={ <Game /> } />
-  ])
+  ]),
+  {
+    basename: '/build/'
+  }
 );
 
 function App() {
