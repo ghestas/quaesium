a3ece0d36d599eab15a65d4ef3a84d96a93b04e0
ghestas <ghestas@ghestas.com>
2024-02-12 13:11:29 +1300

    added fetch request to backend for sets

--- src/Learn.jsx
+++ src/Learn.jsx
@@ -1,8 +1,20 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
 export default function Learn() {
     let { id } = useParams();
+
+    useEffect(() => {
+        async function getSet () {
+            const response = await fetch('https://www.ghestas.com/api/sets?id=1')
+            const responseText = await response.text()
+            console.log(responseText)
+        }
+        getSet()
+    }, [])
+
     return (
-        <div className='learn-set'>
+        <div className='question-set'>
             <h1 className='title'>{id}</h1>
         </div>
     )
