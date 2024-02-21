b562b1a0f696b29df0659ca32e7978b6b2ebe7f7
ghestas <ghestas@ghestas.com>
2024-02-22 08:40:22 +1300

    added basic ui and correct answer detection

--- src/Learn.jsx
+++ src/Learn.jsx
@@ -9,11 +9,13 @@ export default function Learn() {
     const getRandomQuestion = (questionArr) => {
         const index = Math.floor(Math.random() * questionArr.length)
         const randomisedAnswers = questionArr[index].answers.slice().sort(() => Math.random() - 0.6)
-        return {...questionArr[index], answers: randomisedAnswers}
+        return {...questionArr[index], answers: randomisedAnswers, questionIndex: index}
     }
 
-    const handleAnswer = () => {
+    const handleAnswer = (answerIndex) => {
         const newUnfinishedQuestions = unfinishedQuestions.slice()
+        const correctAnswer = unfinishedQuestions[currentQuestion.questionIndex].answers[0]
+        console.log(correctAnswer === currentQuestion.answers[answerIndex])
         newUnfinishedQuestions.splice(unfinishedQuestions.findIndex((e) => {return e.title === currentQuestion.title}), 1)
         setUnfinishedQuestions(newUnfinishedQuestions)
     }
@@ -35,11 +37,15 @@ export default function Learn() {
 
     return (
         <div className='question-set'>
-            <h1 className='title'>{currentQuestion !== null ? currentQuestion.title : ''}</h1>
-            <button className='answer' onClick={handleAnswer}>{currentQuestion !== null ? currentQuestion.answers[0] : ''}</button>
-            <button className='answer' onClick={handleAnswer}>{currentQuestion !== null ? currentQuestion.answers[1] : ''}</button>
-            <button className='answer' onClick={handleAnswer}>{currentQuestion !== null ? currentQuestion.answers[2] : ''}</button>
-            <button className='answer' onClick={handleAnswer}>{currentQuestion !== null ? currentQuestion.answers[3] : ''}</button>
+            <div className='title-container'>
+                <h1 className='title'>{currentQuestion !== null ? currentQuestion.title : ''}</h1>
+            </div>
+            <div className='answer-container'>
+                <button className='answer' onClick={() => {handleAnswer(0)}}>{currentQuestion !== null ? currentQuestion.answers[0] : ''}</button>
+                <button className='answer' onClick={() => {handleAnswer(1)}}>{currentQuestion !== null ? currentQuestion.answers[1] : ''}</button>
+                <button className='answer' onClick={() => {handleAnswer(2)}}>{currentQuestion !== null ? currentQuestion.answers[2] : ''}</button>
+                <button className='answer' onClick={() => {handleAnswer(3)}}>{currentQuestion !== null ? currentQuestion.answers[3] : ''}</button>
+            </div>
         </div>
     );
 }
